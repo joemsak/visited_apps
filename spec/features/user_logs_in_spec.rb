793169bc2c782c756b889c0b075a80a8fe34adc4
Joe Sak <joe@joesak.com>
2014-11-19 13:57:54 -0500

    Ensure in test that user can sign up from the home page

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature 'Users log in' do
+  scenario 'A user signs up' do
+    visit root_path
+
+    click_link 'Sign up'
+
+    fill_in 'Email', with: 'some@email.com'
+    fill_in 'Password', with: 'somesecuresecret'
+    fill_in 'Password confirmation', with: 'somesecuresecret'
+
+    click_button 'Sign up'
+
+    within('.alert-box.notice') do
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+  end
+
   scenario 'A user logs in with correct credentials' do
     user = create(:user)
 
