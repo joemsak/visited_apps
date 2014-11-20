a249b0d24e934932eec977e6c9ec3ec02affbbe2
Joe Sak <joe@joesak.com>
2014-11-19 23:54:37 -0500

    Show user and unassigned apps separately to sort them

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,8 +2,9 @@ class AppsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @apps = App.all
-    @references = @apps.unassigned(current_user).map do |app|
+    @user_apps = current_user.apps
+    @unassigned_apps = App.unassigned(current_user)
+    @references = @unassigned_apps.map do |app|
       { id: app.id, urls: [app.reference_url] }
     end
   end
